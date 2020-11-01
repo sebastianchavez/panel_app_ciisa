b97b9567d995f26d026611d65e44ad3f005bf7b4
Sebastian Chavez <seba.chavez21@gmail.com>
2020-11-01 15:16:35 -0300

    usrs

--- src/app/pages/admins/users/users.component.ts
+++ src/app/pages/admins/users/users.component.ts
@@ -7,12 +7,19 @@ import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import Swal from 'sweetalert2';
 import { SELECTS } from '../../../constants/constants'
 
+import * as XLSX from 'xlsx';
+
+type AOA = any[][];
+
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  data: AOA = [[1, 2], [3, 4]];
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
   idLog = 'UsersComponent';
 
   SELECTS = SELECTS
@@ -128,6 +135,28 @@ export class UsersComponent implements OnInit {
       this.validRut = false;
     }
     console.log(this.validRut)
+  }
+  
+
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[0];
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      /* save data */
+      this.data = <AOA>(XLSX.utils.sheet_to_json(ws, { header: 1 }));
+      console.log(this.data);
+    };
+    reader.readAsBinaryString(target.files[0]);
   }
 
 }
